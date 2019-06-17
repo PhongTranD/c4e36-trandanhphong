3503f7e228f8535ce374ac12a90c779e1cc1b34f
Phong Tran <pierre@MacBook-Pro.local>
2019-06-17 21:57:40 +0700

    abc

---
+++ Session_1/index.js
@@ -0,0 +1,29 @@
+// const x = Number(prompt("nhap x"));
+// const y = Number(prompt("nhap y"));
+
+// console.log(x+y);
+
+// loop
+
+// comment code
+
+// for(let y = 0; y < 5; y++) {
+//     console.log(y);
+//     console.log('ahihi');
+// }
+
+for (let x = 1; x <= 100; x++) {
+    console.log(x);
+}
+
+for (let x = 1; x <= 100; x+=5) {
+    console.log(x);
+}
+
+for (let z = 100; z > 0; z--) {
+    console.log(z);
+}
+
+for (let a = 100; a > 0; a-=3) {
+    console.log(a);
+}
