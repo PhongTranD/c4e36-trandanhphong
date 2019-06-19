d338b9b0920d77854ea69bb2b05c1c2480421e24
PhongTranD <51919656+PhongTranD@users.noreply.github.com>
2019-06-19 10:52:24 +0700

    Add files via upload

--- Session_1/index.js
+++ Session_1/index.js
@@ -1,3 +1,8 @@
+/*
+17/06/2019
+First class
+*/
+
 // const x = Number(prompt("nhap x"));
 // const y = Number(prompt("nhap y"));
 
@@ -12,6 +17,7 @@
 //     console.log('ahihi');
 // }
 
+/*
 for (let x = 1; x <= 100; x++) {
     console.log(x);
 }
@@ -27,5 +33,73 @@ for (let x = 100; x > 0; x--) {
 for (let x = 100; x > 0; x-=3) {
     console.log(x);
 }
+*/
 
-// test
+/**********************************************
+Homework 19/06/2019
+*/
+
+/* 
+1. 
+- How to check variable type?
+console.log(typeof x)
+with x is a pre-defined of undefined variable.
+For example:
+let x = 28
+console.log(typeof x);
+It will show "number"
+or
+let x
+console.log(typeof x);
+It will show "undefined"
+
+- In what cases you will get SyntaxError from the compiler telling you that some of your variables have invalid names?
+When i have an extra or missing character in the name from what JavaScript expects.
+
+Can you give 3 different examples of invalid names?
+let x = 2,8;
+let y = 2P;
+let z = ;6;
+
+2. Write a program that calculates the area of a circle. The circle radius is entered by users
+let x = prompt('Radius?')
+console.log('Area = '+ x * x * 3.14);
+
+3. Write a program that converts Celsius (°C) into Fahrenheit (°F)
+let x = prompt('Enter the temperature in Celsius');
+console.log(x + '°C = ' + ((x * 9 / 5) + 32) + '°F');
+
+4. Use Python Turtle to draw the following shapes
+- A Square
+clear();
+for(i=0;i<4;i++){
+fd(100)
+rt(90)
+};
+
+
+- An equilateral triangle
+clear();
+rt(30)
+for(i=0;i<3;i++){
+fd(200)
+rt(120)
+};
+
+- A circle
+clear();
+for(i=0;i<360;i++){
+fd(2)
+rt(1)
+};
+
+- Multi circles
+clear()
+for(i=0;i<360;i++){
+  for(y=0;y<360;y++){
+    fd(2)
+    rt(1)
+  }
+  rt(1)
+}
+*/
